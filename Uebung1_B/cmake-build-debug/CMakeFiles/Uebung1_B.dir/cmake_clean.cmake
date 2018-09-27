6a5d00a73e9a6aa1e501a34ec7d44468ceae374a
Nino Padrutt <nino.padrutt@hotmail.com>
2018-09-27 11:11:41 +0200

    start with übung 2

---
+++ Uebung1_B/cmake-build-debug/CMakeFiles/Uebung1_B.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Uebung1_B.dir/main.c.obj"
+  "CMakeFiles/Uebung1_B.dir/stack.c.obj"
+  "Uebung1_B.pdb"
+  "Uebung1_B.exe"
+  "Uebung1_B.exe.manifest"
+  "libUebung1_B.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Uebung1_B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Uebung1_B/stack.h
@@ -0,0 +1,25 @@
+//
+// Created by ninop on 27/09/2018.
+//
+
+#ifndef UEBUNG1_B_STACK_H
+#define UEBUNG1_B_STACK_H
+
+#define STACK_SIZE 3
+#define STACK_EMPTY_INDEX -1
+#define STACK_DUMMY_ELEMENT -1234 // attention: arbitrary number used as dummy :-(
+
+typedef int element;
+
+typedef struct {
+    element stackArray[STACK_SIZE];
+    int index;
+} stack;
+
+stack init();
+stack push(element e, stack s);
+element top(stack s);
+stack pop(stack s);
+void print(stack s);
+
+#endif	/* STACK_H */
