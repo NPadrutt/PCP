b70dc44335260c1e4fa91f3684e58f9b203c0f97
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2018-11-15 14:56:17 +0100

    scheme übungen

---
+++ Scheme/Woche 7/Aufgabe 6.bak
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname |Aufgabe 6|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+(define (calc-a-list funcList param1 param2)
+  (cond (empty? funcList) 0)
+      (else
+       (((first funcList) param1 param2 ))
+       )   
+  )
+
+(define rect-calc-list
+(list (lambda (a b) (* a b)) (lambda (a b) (* 2 (+ a b)))))
+
+  (calc-a-list rect-calc-list 2 3)
+  (calc-a-list rect-calc-list 5 5)
--- Scheme/Woche 7/Aufgabe 6.rkt
+++ Scheme/Woche 7/Aufgabe 6.rkt
@@ -1,15 +1,28 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname |Aufgabe 6|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
-(define (calc-a-list funcList param1 param2)
-  (cond (empty? funcList) 0)
-      (else
-       (((first funcList) param1 param2 ))
-       )   
-  )
-
 (define rect-calc-list
-(list (lambda (a b) (* a b)) (lambda (a b) (* 2 (+ a b)))))
+  (list (lambda (a b) (* a b)) (lambda (a b) (* 2 (+ a b))))
+ )
 
-  (calc-a-list rect-calc-list 2 3)
-  (calc-a-list rect-calc-list 5 5)
+(define (calc-a-list funcList param1 param2)
+  (cond
+    ((empty? funcList)
+         (
+          (write "Finished")
+          (newline)
+          )
+         )
+    (else
+     (
+      (write ((first funcList) param1 param2 ))
+      (newline)
+      (calc-a-list (rest funcList) param1 param2)
+      )
+     )
+  )
+)
+
+;(calc-a-list (list) 2 3)
+;(calc-a-list rect-calc-list 2 3)
+(calc-a-list rect-calc-list 5 5)
