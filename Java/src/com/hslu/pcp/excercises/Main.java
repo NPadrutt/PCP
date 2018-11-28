993f0be4cf15595eb6288eddb025b41f44baa155
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2018-11-28 20:25:59 +0100

    start with java excercise

---
+++ Java/src/com/hslu/pcp/excercises/Main.java
@@ -0,0 +1,20 @@
+package com.hslu.pcp.excercises;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+            1a) Es ist möglich, dass das Reduce Null zurückgibt (z.B. wenn names leer ist oder nicht genug Elemente enthält).
+                Null ist eine häufige Fehlerqueller. Daher wird ein Optioanl zurückgegeben. Damit ist klar, dass unter Umständen
+                kein Wert zurück kommt.
+
+         */
+
+    }
+
+
+    public Optional<String> reduceAndPrintStrings(String[] namesArray){
+
+    }
+}
